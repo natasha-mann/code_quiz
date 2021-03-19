f216c89886cda77eb0f9fb147b65d64451e6dc54
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-19 18:03:27 +0000

    added ids to html and linked up with js

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const highScoresButton = document.getElementById("high-score");
+const countdown = document.getElementById("countdown");
+const startQuizButton = document.getElementById("start-button");
