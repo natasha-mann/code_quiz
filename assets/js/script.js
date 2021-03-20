ac48bbbe5037af58a0b71d791aa364e5dcc38da6
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-20 16:32:06 +0000

    added basic timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
-const timer = document.getElementById("timer");
-const startQuizButton = document.getElementById("start-btn");
+const timerSpanElement = document.getElementById("timer");
+const startQuizButtonElement = document.getElementById("start-btn");
+
+let timerValue = 5;
 
 // Timer function
 const startTimer = () => {
   // define callback function for setInterval
   const timerTick = () => {
     // define timer value
+    timerSpanElement.textContent = timerValue;
     // what happens every second
+    timerValue -= 1;
 
     // what happens when no time is left?
-    if (timerunsout) {
+    if (timerValue < 0) {
+      clearInterval(timer);
+      // construct end of quiz container
+
+      // Remove quiz questions container
+
+      // append end of quiz container
+
+      alert("End of Quiz");
     }
   };
   const timer = setInterval(timerTick, 1000);
@@ -22,5 +34,7 @@ const startQuiz = () => {
   // append first question element
   // declare time start value
   // start timer
-  // startTimer();
+  startTimer();
 };
+
+startQuizButtonElement.addEventListener("click", startQuiz);
