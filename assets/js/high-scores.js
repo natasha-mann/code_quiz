984873ae9b8b752f468e6070c1ba3180bbfb90f1
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-20 17:23:12 +0000

    added boilerplate js to highscores js

---
+++ assets/js/high-scores.js
@@ -0,0 +1,13 @@
+const backButtonElement = document.getElementById("back-btn");
+const clearHighScoresButtonElement = document.getElementById(
+  "clear-highscores-btn"
+);
+
+// navigate back to main html document function
+const goBackToQuizPage = () => {};
+
+// clear high scores function
+const clearHighScores = () => {};
+
+backButtonElement.addEventListener("click", goBackToQuizPage);
+clearHighScoresButtonElement.addEventListener("click", clearHighScores);
