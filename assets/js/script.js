b0b75bc2a7b32be14ffabae1a88acd107e013251
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-20 16:22:42 +0000

    added boilerplate js for start quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,26 @@
 const timer = document.getElementById("timer");
 const startQuizButton = document.getElementById("start-btn");
+
+// Timer function
+const startTimer = () => {
+  // define callback function for setInterval
+  const timerTick = () => {
+    // define timer value
+    // what happens every second
+
+    // what happens when no time is left?
+    if (timerunsout) {
+    }
+  };
+  const timer = setInterval(timerTick, 1000);
+};
+
+// Main quiz function
+const startQuiz = () => {
+  // remove intro container
+  // construct quiz container
+  // append first question element
+  // declare time start value
+  // start timer
+  // startTimer();
+};
