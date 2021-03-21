9386ce124244d77959b6debed95c926771962d1a
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-21 20:49:09 +0000

    added clear high scores function

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -2,6 +2,7 @@ const backButtonElement = document.getElementById("back-btn");
 const clearHighScoresButtonElement = document.getElementById(
   "clear-highscores-btn"
 );
+const highScoresContainerDiv = document.getElementById("high-scores");
 const highScoresListContainer = document.getElementById("high-scores-list");
 const highScores = JSON.parse(localStorage.getItem("highScores"));
 
@@ -21,7 +22,6 @@ const constructHighScoresTable = (item, index) => {
   let counter = index + 1;
   const highScoreItem = document.createElement("li");
   highScoreItem.setAttribute("class", "list-item");
-  const highScoreCounter = document.createElement;
   highScoreItem.textContent =
     counter + ".   " + item["Initials"] + " -   " + item["Score"];
   highScoresListContainer.appendChild(highScoreItem);
@@ -33,7 +33,11 @@ const goBackToQuizPage = () => {
 };
 
 // clear high scores function
-const clearHighScores = () => {};
+const clearHighScores = () => {
+  localStorage.clear();
+  highScores.length = 0;
+  highScoresContainerDiv.removeChild(highScoresListContainer);
+};
 
 backButtonElement.addEventListener("click", goBackToQuizPage);
 clearHighScoresButtonElement.addEventListener("click", clearHighScores);
