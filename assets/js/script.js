b22242b47be4d96f7ded85d5d0315c64d44e7f10
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-21 09:14:26 +0000

    constructed game over screen in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,73 @@
 const timerSpanElement = document.getElementById("timer");
 const startQuizButtonElement = document.getElementById("start-btn");
-const introContainer = document.getElementById("intro-container");
+const introContainer = document.getElementById("quiz-container");
 
-let timerValue = 5;
+let timerValue = 1;
+// to remove
+const finalScore = 20;
+
+// Construct quiz questions container
+const constructQuizContainer = () => {
+  const quizContainerDiv = document.createElement("main");
+  quizContainerDiv.setAttribute("class", "quiz-container");
+  quizContainerDiv.setAttribute("id", "quiz-container");
+
+  const questionsContainerDiv = document.createElement("div");
+  questionsContainerDiv.setAttribute("class", "questions-div");
+  questionsContainerDiv.setAttribute("id", "questions-div");
+
+  const answersDiv = document.createElement("div");
+  answersDiv.setAttribute("class", "answers-div");
+  answersDiv.setAttribute("id", "answers-div");
+
+  quizContainerDiv.appendChild(questionsContainerDiv);
+  quizContainerDiv.appendChild(answersDiv);
+
+  return quizContainerDiv;
+};
+
+// Construct Game OVer container
+const constructGameOverContainer = () => {
+  const quizContainerDiv = document.createElement("main");
+  quizContainerDiv.setAttribute("class", "quiz-container");
+  quizContainerDiv.setAttribute("id", "quiz-container");
+
+  const headingContainerDiv = document.createElement("div");
+  headingContainerDiv.setAttribute("class", "results-heading-div");
+
+  const resultsContainerDiv = document.createElement("div");
+  resultsContainerDiv.setAttribute("class", "results-info");
+
+  const finalScoreSpan = document.createElement("span");
+  finalScoreSpan.setAttribute("id", "final-score");
+
+  const gameOverForm = document.createElement("form");
+  gameOverForm.setAttribute("id", "game-over-form");
+
+  const initialsInput = document.createElement("input");
+  initialsInput.setAttribute("placeholder", "Enter Initials");
+  initialsInput.setAttribute("id", "initials-input");
+  initialsInput.setAttribute("class", "initials-input");
+
+  const initialsSubmitBtn = document.createElement("button");
+  initialsSubmitBtn.setAttribute("type", "submit");
+  initialsSubmitBtn.setAttribute("id", "initials-submit-btn");
+  initialsSubmitBtn.setAttribute("class", "initials-submit-btn");
+
+  headingContainerDiv.textContent = "All done";
+  resultsContainerDiv.textContent = "Your final score is: ";
+  // need to define final score
+  finalScoreSpan.textContent = finalScore;
+
+  quizContainerDiv.appendChild(headingContainerDiv);
+  quizContainerDiv.appendChild(resultsContainerDiv);
+  resultsContainerDiv.appendChild(finalScoreSpan);
+  quizContainerDiv.appendChild(gameOverForm);
+  gameOverForm.appendChild(initialsInput);
+  gameOverForm.appendChild(initialsSubmitBtn);
+
+  return quizContainerDiv;
+};
 
 // Timer function
 const startTimer = () => {
@@ -17,52 +82,26 @@ const startTimer = () => {
     if (timerValue < 0) {
       clearInterval(timerInterval);
       // construct end of quiz container
+      const gameOverContainer = constructGameOverContainer();
 
       // Remove quiz questions container
-      const quizContainerDivElement = document.getElementById("quiz-container");
-      document.body.removeChild(quizContainerDivElement);
+      const quizContainerDiv = document.getElementById("quiz-container");
+      document.body.removeChild(quizContainerDiv);
 
       // append end of quiz container
-
-      alert("End of Quiz");
+      document.body.appendChild(gameOverContainer);
     }
   };
   const timerInterval = setInterval(timerTick, 1000);
 };
 
-// Construct quiz questions container
-
-const constructQuizContainer = () => {
-  const quizContainerDivElement = document.createElement("main");
-  quizContainerDivElement.setAttribute("class", "quiz-container");
-  quizContainerDivElement.setAttribute("id", "quiz-container");
-
-  const questionsContainerDiv = document.createElement("div");
-  questionsContainerDiv.setAttribute("class", "questions-div");
-  questionsContainerDiv.setAttribute("id", "questions-div");
-
-  const quizQuestions = document.createElement("h2");
-  quizQuestions.setAttribute("class", "quiz-questions");
-  quizQuestions.setAttribute("id", "quiz-questions");
-
-  const answersDiv = document.createElement("div");
-  answersDiv.setAttribute("class", "answers-div");
-  answersDiv.setAttribute("id", "answers-div");
-
-  const answerButtonsDiv = document.createElement("div");
-  answerButtonsDiv.setAttribute("class", "answer-buttons-div");
-  answerButtonsDiv.setAttribute("id", "answer-buttons-div");
-
-  return quizContainerDivElement;
-};
-
 // Main quiz function
 const startQuiz = () => {
   // construct quiz container
-  const quizContainerDivElement = constructQuizContainer();
+  const quizContainerDiv = constructQuizContainer();
 
   document.body.removeChild(introContainer);
-  document.body.appendChild(quizContainerDivElement);
+  document.body.appendChild(quizContainerDiv);
   // start timer
   startTimer();
 };
