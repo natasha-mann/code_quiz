a4c85d2bd0bd16b3823bcabc9f7d63dd9b37fc20
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-24 22:42:57 +0000

    fixed order scores function

--- assets/js/high-scores.js
+++ assets/js/high-scores.js
@@ -16,10 +16,12 @@ const getHighScoresFromLocalStorage = () => {
 };
 
 // sort scores in descending order
-const orderScores = (highScores) => {
+const orderScores = () => {
+  const highScores = getHighScoresFromLocalStorage();
   highScores.sort(function (a, b) {
-    return parseFloat(b.Score) - parseFloat(a.Score);
+    return parseFloat(b.score) - parseFloat(a.score);
   });
+  return highScores;
 };
 
 // construct high scores table from data stored in local storage
@@ -34,11 +36,8 @@ const constructHighScoresListItem = (item, index) => {
 
 // order high score items and render high score table on screen
 const onLoad = () => {
-  const highScores = getHighScoresFromLocalStorage();
-  if (highScores) {
-    orderScores(highScores);
-    highScores.forEach(constructHighScoresListItem);
-  }
+  const highScores = orderScores();
+  highScores.forEach(constructHighScoresListItem);
 };
 
 window.addEventListener("load", onLoad);
@@ -47,7 +46,6 @@ window.addEventListener("load", onLoad);
 const clearHighScores = () => {
   const highScores = getHighScoresFromLocalStorage();
   localStorage.clear();
-  highScores.length = 0;
   highScoresContainerDiv.removeChild(highScoresListContainer);
 };
 
